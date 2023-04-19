491e84ca03552ffb232bc76b4241308158e8cbd1
frimpsss <afrimpong031@gmail.com>
2023-04-19 00:42:54 +0000

    css tweaks

--- src/components/about-page-components/Card.jsx
+++ src/components/about-page-components/Card.jsx
@@ -5,11 +5,11 @@ const Card = ({ text, subtext, icons }) => {
     <div className="flex flex-row gap-[2rem]">
         {/* bg-[#fff] rounded-[50px] h-[100px] w-[100px] grid place-items-center */}
       <span className="h-fit">
-        <img src={icons} alt="" className="w-[150px]"/>
+        <img src={icons} alt="" className="w-[130px]"/>
       </span>
       <span>
         <h4 className="text-egreen font-medium text-[1.6rem] mb-4">{text}</h4>
-        <p className="font-light text-[1.2rem] pr-[5rem]">{subtext}</p>
+        <p className="font-light text-[1.2rem] lg:pr-[5rem]">{subtext}</p>
       </span>
     </div>
   );
