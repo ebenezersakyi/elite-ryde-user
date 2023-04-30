aa81d9fa24d6249d974bb498c6738f8d8160fa50
frimpsss <afrimpong031@gmail.com>
2023-04-30 14:38:48 +0000

    tweak

--- src/pages/userDashboard/CarsPage.jsx
+++ src/pages/userDashboard/CarsPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CarsPage = () => {
   return (
-    <div className='text-[#fff]'>CarsPage</div>
+    <div className='text-[#fff] 2xl:container 2xl:mx-auto px-[2.5rem]'>CarsPage</div>
   )
 }
 
