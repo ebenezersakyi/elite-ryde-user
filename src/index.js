581248436f5041ea5f55531fb07df06f2b933cfd
frimpsss <afrimpong031@gmail.com>
2023-05-08 09:12:46 +0000

    test auth 1

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <Auth0Provider
+    domain="elite-ryde.us.auth0.com"
+    clientId="dhbmd34m1l593IFmG91ezD4515DVmP5L"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
+  </Auth0Provider>
 );
