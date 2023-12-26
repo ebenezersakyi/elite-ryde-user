010656c57f582e0dd8d73452154d3bfad8e8a6c1
ebenezersakyi <ebensakyi0@gmail.com>
2023-12-26 22:17:18 +0000

    role api

--- src/components/userDashboardComponents/car-page/Rightpane.jsx
+++ src/components/userDashboardComponents/car-page/Rightpane.jsx
@@ -6,9 +6,12 @@ import CarFeatures from "./rightPane/CarFeatures";
 import TimeDetails from "./rightPane/TimeDetails";
 import DriverDetails from "./rightPane/DriverDetails";
 import { Icon } from "@iconify/react";
+import { useSelector } from "react-redux";
 
 const Rightpane = () => {
   const [activeTab, setActiveTab] = useState(0);
+  const { data } = useSelector((d) => d?.selected_car);
+
   return (
     <div className="col-span-2  flex flex-col gap-[2.5rem] scrollbar-hide mt-[90px] sm:mt-0">
       <div className="flex w-[100%] sm:hidden pt-[25px] border-t-[1px] border-t-[#cdcdcd]">
@@ -68,20 +71,22 @@ const Rightpane = () => {
             className={`${activeTab == 3 ? "text-egreen" : "text-[#FFF]"}`}
           />
         </span>
-        <span
-          className={`${
-            activeTab == 4 && "border-b-[3px] border-b-egreen"
-          } w-[25%] flex p-[3px] pb-[10px] justify-center items-center`}
-          onClick={() => {
-            setActiveTab(4);
-          }}
-        >
-          <Icon
-            icon="mdi:user"
-            width={30}
-            className={`${activeTab == 4 ? "text-egreen" : "text-[#FFF]"}`}
-          />
-        </span>
+        {data?.driver && (
+          <span
+            className={`${
+              activeTab == 4 && "border-b-[3px] border-b-egreen"
+            } w-[25%] flex p-[3px] pb-[10px] justify-center items-center`}
+            onClick={() => {
+              setActiveTab(4);
+            }}
+          >
+            <Icon
+              icon="mdi:user"
+              width={30}
+              className={`${activeTab == 4 ? "text-egreen" : "text-[#FFF]"}`}
+            />
+          </span>
+        )}
       </div>
 
       <div className={`${activeTab == 0 ? "block" : "hidden"} sm:block`}>
@@ -104,11 +109,13 @@ const Rightpane = () => {
           <TimeDetails />
         </SectionLayout>
       </div>
-      <div className={`${activeTab == 4 ? "block" : "hidden"} sm:block`}>
-        <SectionLayout title={"Driver Details"}>
-          <DriverDetails />
-        </SectionLayout>
-      </div>
+      {data?.driver && (
+        <div className={`${activeTab == 4 ? "block" : "hidden"} sm:block`}>
+          <SectionLayout title={"Driver Details"}>
+            <DriverDetails />
+          </SectionLayout>
+        </div>
+      )}
     </div>
   );
 };
